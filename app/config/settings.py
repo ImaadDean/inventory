8a5cf5ac66d293ef10ad91e746cc11c42f65c6a2
ImaadDean <123092643+ImaadDean@users.noreply.github.com>
2025-08-09 23:19:59 +0600

    Update settings.py changing database to perfumesandmore

--- app/config/settings.py
+++ app/config/settings.py
@@ -19,7 +19,7 @@ class Settings(BaseSettings):
     MONGO_HOST: str = "144.21.56.184"
     MONGO_PORT: int = 27017
     
-    MONGO_DATABASE: str = "inventory"
+    MONGO_DATABASE: str = "perfumesandmore"
     MONGO_POOL_SIZE: int = 20
 
     # JWT Configuration
